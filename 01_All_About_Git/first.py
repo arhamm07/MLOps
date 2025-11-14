c1818d94922a14fa35af9560570be2541e866250
arham <arhamop937@gmail.com>
2025-11-14 18:03:55 +0500

    Everything About Git

---
+++ 01_All_About_Git/first.py
@@ -0,0 +1,8 @@
+a = 'This is a Git tutorial'
+
+b = 'Git is a version control system'
+
+print(a)
+print(b)
+
+str2 = 'This is a string for final'
---
+++ 01_All_About_Git/helper2.py
@@ -0,0 +1,3 @@
+print('This is a helper file')
+
+print("This is a 2nd branch")
